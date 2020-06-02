947c0e4f7b9b59534458e6ca9bafd19eb3a2bb45
farleybsd <farley.t.i@hotmail.com>
2020-06-02 16:50:49 -0300

    Autenticação feita pelo Laravel

--- ControleSeries/routes/web.php
+++ ControleSeries/routes/web.php
@@ -23,3 +23,7 @@ Route::post('/series/{id}/editaNome', 'SeriesController@editaNome');
 
 Route::get('/temporadas/{temporada}/episodios', 'EpisodiosController@index');
 Route::post('/temporadas/{temporada}/episodios/assistir', 'EpisodiosController@assistir');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
