0ba489d33bafd5d11a7886652c412e6364f9dd7f
farleybsd <farley.t.i@hotmail.com>
2020-06-01 22:04:13 -0300

    Mostrando para o usuário

--- ControleSeries/app/Http/Controllers/EpisodiosController.php
+++ ControleSeries/app/Http/Controllers/EpisodiosController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 
 class EpisodiosController extends Controller
 {
-    public function index(Temporada $temporada) // esse paramento vem da rota
+    public function index(Temporada $temporada, Request $request) // esse paramento vem da rota
     {
         //$episodios = $temporada->episodios;
         // echo "<pre>";
@@ -17,7 +17,8 @@ class EpisodiosController extends Controller
         $temporadaId = $temporada->id;
         return view('episodios.index', [
             'episodios' => $temporada->episodios,
-            'temporadaId' => $temporada->id
+            'temporadaId' => $temporada->id,
+            'mensagem' => $request->session()->get('mensagem')
         ]);
     }
 
@@ -32,5 +33,8 @@ class EpisodiosController extends Controller
         }); // each e tipo um cursos ele execulta uma funcao passando o seu valor de parametro varivas vezes
 
         $temporada->push();
+        //return redirect('/temporadas/' . $temporada->id . '/episodios');
+        $request->session()->flash('mensagem', 'Episódios Marcados como assistidos');
+        return redirect()->back();
     }
 }
