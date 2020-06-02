b4e6a1cbe6ee27893d9ea8a139dfe2356511fda9
farleybsd <farley.t.i@hotmail.com>
2020-06-02 14:53:51 -0300

    Exibindo os episódios assistidos

--- ControleSeries/app/Temporada.php
+++ ControleSeries/app/Temporada.php
@@ -1,23 +1,34 @@
 <?php
+
 namespace App;
+
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
-    class Temporada extends Model
+class Temporada extends Model
+{
+
+    protected $fillable = ['numero'];
+    public $timestamps = false;
+
+    public function episodios()
+    {
+        return $this->hasMany(Episodio::class); //muitos episodios
+    }
+
+    //Relacionamento
+    public function serie()
     {
 
-        protected $fillable = ['numero'];
-        public $timestamps = false;
+        return $this->belongsTo(Serie::class); // Temporada pertence a uma serie
 
-        public function episodios()
-        {
-            return $this->hasMany(Episodio::class);//muitos episodios
-        }
-
-        //Relacionamento
-        public function serie()
-        {
-
-            return $this->belongsTo(Serie::class); // Temporada pertence a uma serie
-
-        }
     }
+
+    public function getEpisodiosAssistidos(): Collection
+    {
+        return $this->episodios->filter(function (Episodio $episodio) {
+
+            return $episodio->assistido;
+        });
+    }
+}
