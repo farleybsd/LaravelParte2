06f9a7df2dc2c07ddb5d7cb700f4d86191806fc7
farleybsd <farley.t.i@hotmail.com>
2020-06-01 21:34:26 -0300

    Assistindo os episódios

--- ControleSeries/app/Http/Controllers/EpisodiosController.php
+++ ControleSeries/app/Http/Controllers/EpisodiosController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-
+use App\Episodio;
 use App\Temporada;
 use Illuminate\Http\Request;
 
@@ -10,10 +10,27 @@ class EpisodiosController extends Controller
 {
     public function index(Temporada $temporada) // esse paramento vem da rota
     {
-        $episodios = $temporada->episodios;
+        //$episodios = $temporada->episodios;
         // echo "<pre>";
         // var_dump($episodios);
         // echo "</pre>";
-        return view('episodios.index', compact('episodios'));
+        $temporadaId = $temporada->id;
+        return view('episodios.index', [
+            'episodios' => $temporada->episodios,
+            'temporadaId' => $temporada->id
+        ]);
+    }
+
+    public function assistir(Temporada $temporada, Request $request)
+    {
+        //var_dump($request->episodios); //name do input do html
+        $episodioAssistido = $request->episodios;
+        $temporada->episodios->each(function (Episodio $episodio) use ($episodioAssistido) {
+            $episodio->assistido = in_array($episodio->id, $episodioAssistido); //coluna do banco
+
+            // $episodio->save();
+        }); // each e tipo um cursos ele execulta uma funcao passando o seu valor de parametro varivas vezes
+
+        $temporada->push();
     }
 }
